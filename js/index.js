cce78ea3cde4e9cc6b28984f2c2be402c6b6aaaf
M.Refaei Hannan <56527662+REFAEI91@users.noreply.github.com>
2023-03-21 12:37:58 +0100

    new version

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const questionCards = document.querySelectorAll(".question-card");
+
+questionCards.forEach((card) => {
+  const showAnswerButton = card.querySelector(".show-answer-button");
+  const answer = card.querySelector(".answer");
+
+  showAnswerButton.addEventListener("click", () => {
+    if (answer.style.display === "none") {
+      answer.style.display = "block";
+      showAnswerButton.innerText = "Hide Answer";
+    } else {
+      answer.style.display = "none";
+      showAnswerButton.innerText = "Show Answer";
+    }
+  });
+
+  const bookmarkButton = card.querySelector(".bookmark-button");
+
+  bookmarkButton.addEventListener("click", () => {
+    bookmarkButton.classList.toggle("bookmarked");
+  });
+});
+
+let toggle = false;
+const bookmarkBtn = document.querySelectorAll('[data-js="bookmark-button"]');
+function changeImage() {
+  if (toggle === true) {
+    bookmarkBtn.src = "img/bookmark.png";
+  } else {
+    bookmarkBtb.src = "img/book.png";
+  }
+  toggle = !toggle;
+}
+
+const homepageButton = document.querySelector(".homepage-button");
+const profileButton = document.querySelector(".profile-button");
+const bookmarkPageButton = document.querySelector(".bookmark-page-button");
+
+homepageButton.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+
+profileButton.addEventListener("click", () => {
+  window.location.href = "profile.html";
+});
+
+bookmarkPageButton.addEventListener("click", () => {
+  window.location.href = "Bookmark.html";
+});
